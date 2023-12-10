5b10fc259336afc583255352eff6ee0109be39b9
john maliha <johnmaliha@hotmail.com>
2023-12-09 20:56:05 -0500

    working cluster(esh)

--- terraform/mysql_cluster_manager.sh
+++ terraform/mysql_cluster_manager.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 exec > /home/ubuntu/logs.log 2>&1 # tail -f logs.log
 
+# this is the commun code section
 # Update and install dependencies
 sudo apt update -y
-sudo apt upgrade -y
-sudo apt-get install -y libncurses5 sysbench unzip
-
-# Add MySQL user if not exists
-if ! id "mysql" &>/dev/null; then
-    sudo useradd -r -s /bin/false mysql
-fi
+sudo apt-get install -y sysbench unzip
 
 # Download and install MySQL Cluster
 mkdir -p /opt/mysqlcluster/home
@@ -21,23 +16,28 @@ ln -s mysql-cluster-gpl-7.2.1-linux2.6-x86_64 mysqlc
 # Set environment variables
 echo 'export MYSQLC_HOME=/opt/mysqlcluster/home/mysqlc' > /etc/profile.d/mysqlc.sh
 echo 'export PATH=$MYSQLC_HOME/bin:$PATH' >> /etc/profile.d/mysqlc.sh
-echo 'export PATH=/opt/mysqlcluster/home/mysqlc/bin:$PATH' >> /opt/mysqlcluster/home/mysqlc/bin
 source /etc/profile.d/mysqlc.sh
+sudo apt-get update && sudo apt-get -y install libncurses5
 
+# manager code
 # Create directories and configuration files
-mkdir -p /opt/mysqlcluster/deploy/{conf,mysqld_data,ndb_data}
-cd /opt/mysqlcluster/deploy/conf
+mkdir -p /opt/mysqlcluster/deploy
+cd /opt/mysqlcluster/deploy
+mkdir conf
+mkdir mysqld_data
+mkdir ndb_data
+cd conf 
 
 # Create the my.cnf file
-echo "[mysqld]
+echo -e "
+[mysqld]
 ndbcluster
 datadir=/opt/mysqlcluster/deploy/mysqld_data
 basedir=/opt/mysqlcluster/home/mysqlc
 port=3306" > my.cnf
 
 # create the config.ini
-echo "
-# for the master
+echo -e "# for the master
 [ndb_mgmd]
 hostname=ip-172-31-42-0.ec2.internal
 datadir=/opt/mysqlcluster/deploy/ndb_data
@@ -63,49 +63,22 @@ hostname=ip-172-31-42-3.ec2.internal
 nodeid=4
 
 [mysqld]
-nodeid=50
-" > config.ini
-
-# Start management node
-sudo /opt/mysqlcluster/home/mysqlc/bin/ndb_mgmd -f /opt/mysqlcluster/deploy/conf/config.ini --initial --configdir=/opt/mysqlcluster/deploy/conf || exit 1
-
-# Initialize the database
-# sudo /opt/mysqlcluster/home/mysqlc/scripts/mysql_install_db --no-defaults --datadir=/opt/mysqlcluster/deploy/mysqld_data
-sudo /opt/mysqlcluster/home/mysqlc/scripts/mysql_install_db --no-defaults --basedir=/opt/mysqlcluster/home/mysqlc --datadir=/opt/mysqlcluster/deploy/mysqld_data
-
+nodeid=50" > config.ini
 
 # Start MySQL Server
 cd /opt/mysqlcluster/home/mysqlc
+# Initialize the database
 sudo scripts/mysql_install_db --no-defaults --datadir=/opt/mysqlcluster/deploy/mysqld_data
-# sudo /opt/mysqlcluster/home/mysqlc/bin/mysqld_safe &
 
-# create ngmd 
-# sudo nano /etc/systemd/system/ndb_mgmd.service
-echo "[Unit]
-Description=MySQL Cluster Management Node
-After=network.target
+sudo chown -R mysql:mysql /opt/mysqlcluster/home/mysqlc
 
-[Service]
-Type=forking
-ExecStart=/opt/mysqlcluster/home/mysqlc/bin/ndb_mgmd -f /opt/mysqlcluster/deploy/conf/config.ini --configdir=/opt/mysqlcluster/deploy/conf
-ExecStop=/opt/mysqlcluster/home/mysqlc/bin/ndb_mgmd --shutdown
-User=mysql
-Restart=on-failure
+# Start management node
+sudo /opt/mysqlcluster/home/mysqlc/bin/ndb_mgmd -f /opt/mysqlcluster/deploy/conf/config.ini --initial --configdir=/opt/mysqlcluster/deploy/conf/
 
-[Install]
-WantedBy=multi-user.target" > /etc/systemd/system/ndb_mgmd.service
+# check status of management data nodes
+ndb_mgm -e show
 
-# restart then start the ndb_mgmd
-sudo systemctl daemon-reload
-sudo systemctl enable ndb_mgmd
-sudo systemctl start ndb_mgmd
-sudo systemctl status ndb_mgmd
+# start the sql node
+mysqld --defaults-file=/opt/mysqlcluster/deploy/conf/my.cnf --user=root &
 
-# Wait for MySQL Server to start (better check)
-sleep 10
-
-# Download and extract Sakila DB
-wget https://downloads.mysql.com/docs/sakila-db.zip
-unzip sakila-db.zip -d /db
-
-# Load Sakila Database
+ndb_mgm -e show
--- terraform/mysql_cluster_workers.sh
+++ terraform/mysql_cluster_workers.sh
@@ -1,73 +1,50 @@
 #!/bin/bash
 exec > /home/ubuntu/logs.log 2>&1 # tail -f logs.log
 
-# Inspired from this link : https://stansantiago.wordpress.com/2012/01/04/installing-mysql-cluster-on-ec2/
-
-cd ~;
-#installing dependencies
+# this is the commun code section
+# Update and install dependencies
 sudo apt update -y
-sudo apt upgrade -y
-sudo apt-get install -y libncurses5
-sudo apt-get install sysbench
-sudo apt-get install -y unzip
+sudo apt-get install -y sysbench
 
-# Download and then install MySQL cluster
+# Download and install MySQL Cluster
 mkdir -p /opt/mysqlcluster/home
 cd /opt/mysqlcluster/home
 wget http://dev.mysql.com/get/Downloads/MySQL-Cluster-7.2/mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz
 tar xvf mysql-cluster-gpl-7.2.1-linux2.6-x86_64.tar.gz
 ln -s mysql-cluster-gpl-7.2.1-linux2.6-x86_64 mysqlc
 
+# Set environment variables
 echo 'export MYSQLC_HOME=/opt/mysqlcluster/home/mysqlc' > /etc/profile.d/mysqlc.sh
 echo 'export PATH=$MYSQLC_HOME/bin:$PATH' >> /etc/profile.d/mysqlc.sh
-source /etc/profile.d/mysqlc.sh 
-
-# Create the Deployment Directory and Setup Config Files
-mkdir -p /opt/mysqlcluster/deploy
-cd /opt/mysqlcluster/deploy
-mkdir conf
-mkdir mysqld_data
-mkdir ndb_data
-cd conf
-
-# Create the my.cnf file
-echo "[mysqld]
-ndbcluster
-datadir=/opt/mysqlcluster/deploy/mysqld_data
-basedir=/opt/mysqlcluster/home/mysqlc
-port=3306" > my.cnf
-
-# create the config.ini
-echo "
-# for the master
-[ndb_mgmd]
-hostname=ip-172-31-42-0.ec2.internal
-datadir=/opt/mysqlcluster/deploy/ndb_data
-nodeid=1
-
-[ndbd default]
-noofreplicas=3
-datadir=/opt/mysqlcluster/deploy/ndb_data
-
-# for slave #1
-[ndbd]
-hostname=ip-172-31-42-1.ec2.internal
-nodeid=2
-
-#for slave #2
-[ndbd]
-hostname=ip-172-31-42-2.ec2.internal
-nodeid=3
-
-#for slave #3
-[ndbd]
-hostname=ip-172-31-42-3.ec2.internal
-nodeid=4
-
-[mysqld]
-nodeid=50
-" > config.ini
+source /etc/profile.d/mysqlc.sh
+sudo apt-get update && sudo apt-get -y install libncurses5
 
 mkdir -p /opt/mysqlcluster/deploy/ndb_data
-# start up Data Node
-ndbd -c ip-172-31-42-0.ec2.internal
+# start up Data Node with the address of the manager
+ndbd -c ip-172-31-42-0.ec2.internal:1186 # 1186 default port 
+
+# MAX_ATTEMPTS=30
+# ATTEMPT=0
+# SUCCESS=false
+
+# # Loop to check status
+# while [ $ATTEMPT -lt $MAX_ATTEMPTS ]; do
+#     # Check status
+#     if ndb_mgm -e show | grep -q "connected"; then
+#         SUCCESS=true
+#         break
+#     fi
+
+#     ATTEMPT=$((ATTEMPT+1))
+#     sleep 10
+# done
+
+# if [ "$SUCCESS" = true ]; then
+#     echo "All nodes are successfully connected."
+# else
+#     echo "Failed to connect all nodes within the expected time."
+#     exit 1
+# fi
+
+# check status of management data nodes
+ndb_mgm -e show
